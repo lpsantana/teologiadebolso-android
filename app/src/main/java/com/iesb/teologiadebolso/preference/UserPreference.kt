f9fe1bbf5c002a68c4a649275d69f1c21f4a2180
Junio <a.junio.fc@gmail.com>
2019-04-08 17:37:48 -0300

    Criar arquivo de preferencias pra salvar os dados do usuario logado

---
+++ app/src/main/java/com/iesb/teologiadebolso/preference/UserPreference.kt
@@ -0,0 +1,37 @@
+package com.iesb.teologiadebolso.preference
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class UserPreference(private val context: Context) {
+    private val FILE_NAME = "com.iesb.teologiadebolso.preference"
+    private val USER_LOGIN_KEY = "USER_LOGIN_KEY"
+    private lateinit var preference: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+    init {
+        preference = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        editor = preference.edit()
+    }
+
+    fun saveUser(email: String){
+        editor.putString(USER_LOGIN_KEY, email)
+        editor.commit()
+    }
+
+    fun getCurrentUser(): String{
+        return preference.getString(USER_LOGIN_KEY, null)
+    }
+
+    fun isLogged(): Boolean {
+        if (getCurrentUser() == null){
+            return false
+        }
+        return true
+    }
+
+    fun logout(){
+        editor.clear()
+        editor.commit()
+    }
+}
