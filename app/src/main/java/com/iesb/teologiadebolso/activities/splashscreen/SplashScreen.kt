d0169692b978ea975c733092527515627d5ccf8e
lp_santana@hotmail.com <Leon@rdo!00>
2019-05-21 19:45:28 -0300

    Splash screen

--- app/src/main/java/com/iesb/teologiadebolso/activities/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/iesb/teologiadebolso/activities/splashscreen/SplashScreen.kt
@@ -1,23 +1,45 @@
 package com.iesb.teologiadebolso.activities.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
 import com.iesb.teologiadebolso.R
-
-import kotlinx.android.synthetic.main.activity_splash_screen.*
+import com.iesb.teologiadebolso.activities.login.LoginActivity
 
 class SplashScreen : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
         }
+
+        super.onDestroy()
     }
 
 }
