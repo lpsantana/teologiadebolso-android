cd93551ca9fd0644b22bf54830a5a546cfcd0e7e
Junio <a.junio.fc@gmail.com>
2019-05-21 20:39:35 -0300

    Criar activity de onboarding

---
+++ app/src/main/java/com/iesb/teologiadebolso/activities/onboarding/OnboardingActivity.kt
@@ -0,0 +1,13 @@
+package com.iesb.teologiadebolso.activities.onboarding
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.iesb.teologiadebolso.R
+
+class OnboardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
