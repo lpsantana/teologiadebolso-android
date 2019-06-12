2e43b3c5e25ec3e8ad80095e2a21938831553639
Junio <a.junio.fc@gmail.com>
2019-06-11 22:42:00 -0300

    Finalizar xml de editar perfil

---
+++ app/src/main/java/com/iesb/teologiadebolso/activities/perfil/EditarPerfilActivity.kt
@@ -0,0 +1,13 @@
+package com.iesb.teologiadebolso.activities.perfil
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.iesb.teologiadebolso.R
+
+class EditarPerfilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_perfil)
+    }
+}
